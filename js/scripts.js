b466d01b5094ffa26f92365ff8319459c4bfdb73
Jose <jose02460@gmail.com>
2017-12-01 16:11:15 -0800

    edit html and scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
 $(document).ready(function() {
 //  $('#actor-Form').validator();
     $("form#track-Form").validator().on('submit', function(event) {
-      var age = parseInt($("#inputAge").val());
-      var food = $("#question").val();
-      var sex = $("input:radio[name=sex]:checked").val();
-      if((question === "Sushi" || question === "Pizza" || question === "Noodles")){
-        //$(".animalName-title").text(animal);
-        $("#actors").show();
+      if((question === "Yes" || question === "Yes" || question === "No" || question === "No" || question === "No")){
+        $("#track").show();
+        $("#ruby").show();
+        $("java").hide();
+        $("#cc").hide();
+      }
+      if(question === "Yes" || question === "Yes" || question === "Yes" || question === "Yes" question === "Yes")){
+        $("#track").show();
         $("#ruby").hide();
+        $("java").show();
+        $("#cc").hide();
+      }
+      if(question === "Yes" || question === "Yes" || question === "Yes" || question === "Yes" question === "No")){
+        $("#track").show();
+        $("#ruby").hide();
+        $("java").hide();
         $("#cc").show();
       }
-      if(question === "yes" || question === "yes" || question === "yes" || question === "yes" question === "yes")){
-        //$(".animalName-title").text(animal);
-        $("#actors").show();
-        $("#cc").hide();
-        $("#ruby").show();
-      }
       event.preventDefault();
+    });
   });
 });
