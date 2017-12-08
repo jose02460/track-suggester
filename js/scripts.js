c6fe4a069afeb2d8761a76fd0cd320212eb53a95
Guest User <Guest@Epicodus-8C.local>
2017-12-07 16:18:20 -0800

    update front end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,29 @@
 $(document).ready(function() {
-    $('#track-Form').validator();
-    $("form#track-Form").validator().on('submit', function(event) {
-      if((question === "Yes" || question === "Yes" || question === "No" || question === "No" || question === "No")){
+    $("form#track-Form").submit(function(event) {
+      event.preventDefault();
+      var question1 = $("#question1").val();
+      var question2 = $("#question2").val();
+      var question3 = $("#question3").val();
+      var question4 = $("#question4").val();
+      var question5 = $("#question5").val();
+
+      if(question1 === "Yes" || question2 === "Yes" || question3 === "No" || question4 === "No" || question5 === "No") {
         $("#track").show();
         $("#ruby").show();
-        $("java").hide();
+        $("#java").hide();
         $("#cc").hide();
-      }
-      if(question === "Yes" || question === "Yes" || question === "Yes" || question === "Yes" question === "Yes")){
+      };
+      if(question1 === "Yes" || question2 === "Yes" || question3 === "Yes" || question4 === "Yes" || question5 === "Yes") {
         $("#track").show();
         $("#ruby").hide();
-        $("java").show();
+        $("#java").show();
         $("#cc").hide();
-      }
-      if(question === "Yes" || question === "Yes" || question === "Yes" || question === "Yes" question === "No")){
+      };
+      if(question1 === "Yes" || question2 === "Yes" || question3 === "Yes" || question4 === "Yes" || question5 === "No") {
         $("#track").show();
         $("#ruby").hide();
-        $("java").hide();
+        $("#java").hide();
         $("#cc").show();
-      }
-      event.preventDefault();
+      };
     });
   });
-});
